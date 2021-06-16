5b4f4e609ca32f5a245baa985a882de0ed0f3058
bartosz0 <methkill@gmail.com>
2021-06-16 11:32:28 +0200

    Added data,order and user classes

---
+++ Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsmanskiQueryEngine
+{
+    public class Order
+    {
+        public string Name;
+        public int Number;
+    }
+}
---
+++ User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsmanskiQueryEngine
+{
+    public class User
+    {
+        public string Email;
+        public string FullName;
+        public int Age;
+    }
+}
