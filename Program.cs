a34cd87003976d605337bfa84b6a67c286ed3fc9
bartosz0 <methkill@gmail.com>
2021-06-16 13:02:56 +0200

    Added LINQ query

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MySqlX.XDevAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace OsmanskiQueryEngine
 {
@@ -6,7 +9,35 @@ namespace OsmanskiQueryEngine
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            List<User> usersList = new();
+
+            var user1 = new User("abc@wp.pl", "John wick", 31);
+            var user2 = new User("bjaksd@wp.pl", "Gandalf", 69);
+            var user3 = new User("njdald@wp.pl", "Luke Skywalker", 29);
+
+            usersList.Insert(0, user1);
+            usersList.Insert(1, user2);
+            usersList.Insert(2, user3);
+
+            var newUser = from User in usersList
+                          where User.Age > 30 && User.FullName == "John wick"
+                          select User.Email;
+
+            // Błąd ;/
+            //IEnumerable<User> newUser = (IEnumerable<User>)(from User in usersList
+            //                                                where User.FullName == "John wick" && User.Age > 30
+            //                                                select User.Email);
+
+            //IEnumerable<User> newUser = from x in usersList.Where(x => x.FullName == "John Wick" || x.Email == "abc@wp.pl" || x.Age == 30)
+            //              .Select(x => new User { Email = x.Email, FullName = x.FullName, Age = x.Age });
+
+            foreach (var item in newUser)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
         }
     }
 }
--- User.cs
+++ User.cs
@@ -8,6 +8,13 @@ namespace OsmanskiQueryEngine
 {
     public class User
     {
+        public User(string Email, string FullName, int Age)
+        {
+            this.Email = Email;
+            this.FullName = FullName;
+            this.Age = Age;
+        }
+
         public string Email;
         public string FullName;
         public int Age;
